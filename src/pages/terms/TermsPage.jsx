fe9328da97217d0b557535a3573b005a160500e7
Alyorbekjonxon <alyorbek391@gmail.com>
2025-03-13 17:58:46 +0500

    yabgilanish

--- src/pages/terms/TermsPage.jsx
+++ src/pages/terms/TermsPage.jsx
@@ -1,20 +1,19 @@
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux';
-import { translations } from '../../data';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { translations } from "../../data";
+import { useLocation } from "react-router-dom";
 
 const TermsPage = () => {
-    const language = useSelector((state) => state.language.language);
+  const language = useSelector((state) => state.language.language);
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
-    
 
   return (
     <section id="condition" className="pt-[60px] pb-[60px]">
-      <div className="container mx-auto px-[20px]" >
+      <div className="container mx-auto px-[20px]">
         <h1 className="text-white text-[24px] mb-[20px] font-[500]">
           {translations[language]?.termsTitle || translations?.en?.termsTitle}
         </h1>
@@ -29,7 +28,7 @@ const TermsPage = () => {
           {translations[language]?.termsRow1Desc ||
             translations?.en?.termsRow1Desc}
         </p>
-        <h1 className='text-white text-[24px] mb-[10px] font-[500]'>
+        <h1 className="text-white text-[24px] mb-[10px] font-[500]">
           {translations[language]?.termsRow2Title ||
             translations?.en?.termsRow2Title}
         </h1>
@@ -37,7 +36,7 @@ const TermsPage = () => {
           {translations[language]?.termsRow2Desc ||
             translations?.en?.termsRow2Desc}
         </p>
-        <h1 className='text-white text-[24px] mb-[10px] font-[500]'>
+        <h1 className="text-white text-[24px] mb-[10px] font-[500]">
           {translations[language]?.termsRow3Title ||
             translations?.en?.termsRow3Title}
         </h1>
@@ -45,7 +44,7 @@ const TermsPage = () => {
           {translations[language]?.termsRow3Desc ||
             translations?.en?.termsRow3Desc}
         </p>
-        <h1 className='text-white text-[24px] mb-[10px] font-[500]'>
+        <h1 className="text-white text-[24px] mb-[10px] font-[500]">
           {translations[language]?.termsRow4Title ||
             translations?.en?.termsRow4Title}
         </h1>
@@ -53,7 +52,7 @@ const TermsPage = () => {
           {translations[language]?.termsRow4Desc ||
             translations?.en?.termsRow4Desc}
         </p>
-        <h1 className='text-white text-[24px] mb-[10px] font-[500]'>
+        <h1 className="text-white text-[24px] mb-[10px] font-[500]">
           {translations[language]?.termsRow5Title ||
             translations?.en?.termsRow5Title}
         </h1>
@@ -64,6 +63,6 @@ const TermsPage = () => {
       </div>
     </section>
   );
-}
+};
 
-export default TermsPage
+export default TermsPage;
